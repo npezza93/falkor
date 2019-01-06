086960e2880385dba1b5017e644fe805b2983835
Nick Pezza <npezza93@gmail.com>
2019-01-06 17:28:22 -0500

    Add load_yardoc

--- lib/falkor.rb
+++ lib/falkor.rb
@@ -5,6 +5,7 @@ require "falkor/download"
 require "falkor/download/ruby"
 require "falkor/gunzip"
 require "falkor/generate_yardoc"
+require "falkor/load_yardoc"
 
 module Falkor
   class Error < StandardError; end
---
+++ lib/falkor/load_yardoc.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "yard"
+
+module Falkor
+  class LoadYardoc
+    def initialize(yardoc_file_path)
+      @store = YARD::RegistryStore.new
+      store.load(yardoc_file_path)
+    end
+
+    %i(root class method module constant classvariable macro).each do |type|
+      define_method("values_for_#{type}") do
+        store.values_for_type(type)
+      end
+    end
+
+    private
+
+    attr_accessor :store
+  end
+end
