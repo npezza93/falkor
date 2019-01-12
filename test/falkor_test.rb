3818f821333446271cdd1f9a9e1f0de5a905ffd1
Nick Pezza <npezza93@gmail.com>
2019-01-11 21:08:43 -0500

    Remove test failure

--- test/falkor_test.rb
+++ test/falkor_test.rb
@@ -6,8 +6,4 @@ class FalkorTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Falkor::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
