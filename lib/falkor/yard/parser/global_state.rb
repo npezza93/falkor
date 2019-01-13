ac4ad85fa1cc4c88cb36d0c136dcd33797c11092
Nick Pezza <npezza93@gmail.com>
2019-01-13 11:19:47 -0500

    Reorganize and rename yardoc related classes

---
+++ lib/falkor/yard/parser/global_state.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Falkor
+  module Yard
+    class Parser
+      class GlobalState
+        attr_accessor :ordered_parser, :total_processed, :cruby_processed_files,
+                      :cruby_namespaces, :cruby_symbols,
+                      :cruby_override_comments, :method_count, :block
+
+        def initialize(ordered_parser, total_processed)
+          @ordered_parser = ordered_parser
+          @total_processed = total_processed
+        end
+
+        def processed(file_name)
+          return if block.nil?
+
+          block.call(1, "Parsed #{file_name}")
+        end
+      end
+    end
+  end
+end
