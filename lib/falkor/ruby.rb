cc1feec380d22a4f758829fde9ce00f4efcffda7
Nick Pezza <npezza93@gmail.com>
2019-10-18 21:49:09 -0400

    Update ruby to read from ruby releases page

--- lib/falkor/ruby.rb
+++ lib/falkor/ruby.rb
@@ -8,23 +8,21 @@ module Falkor
   class Ruby
     include Installable
 
-    VERSIONS = {
-      "2.6.1" =>
-        "https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.1.tar.gz",
-      "2.6.0" =>
-        "https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.0.tar.gz",
-      "2.5.3" =>
-        "https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.3.tar.gz",
-      "2.4.5" =>
-        "https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.5.tar.gz",
-      "2.3.8" =>
-        "https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.8.tar.gz",
-    }.freeze
+    RELEASES = "https://raw.githubusercontent.com/ruby/www.ruby-lang.org/"\
+               "master/_data/releases.yml"
+
+    def self.versions
+      YAML.
+        load_file(Download.new(RELEASES, "ruby_releases.yml").download {}).
+        select { |release| release.dig("url", "gz") }.
+        map { |release| [release["version"], release.dig("url", "gz")] }.
+        to_h
+    end
 
     def self.search(query)
       return [] if query.nil? || query.empty?
 
-      VERSIONS.keys.map do |version|
+      versions.keys.map do |version|
         next unless version.include? query
 
         new(version)
@@ -36,13 +34,13 @@ module Falkor
     end
 
     def initialize(version)
-      raise VersionNotFound if VERSIONS[version].nil?
+      raise VersionNotFound if self.class.versions[version].nil?
 
       @version = version
     end
 
     def other_versions
-      VERSIONS.keys.map do |number|
+      self.class.versions.keys.map do |number|
         next if number == version
 
         self.class.new(number)
@@ -57,14 +55,14 @@ module Falkor
 
     private
 
-    attr_accessor :version
+    attr_reader :version
 
     def file_name
       "ruby-#{version}.tar.gz"
     end
 
     def url
-      VERSIONS[version]
+      self.class.versions[version]
     end
 
     def yard_filepath
