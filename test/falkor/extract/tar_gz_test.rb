d2e369b2c6bbe5203fc35164fbbc2a619038d169
Nick Pezza <npezza93@gmail.com>
2019-10-20 09:41:23 -0400

    Test Falkor::Extract::TarGz

---
+++ test/falkor/extract/tar_gz_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Falkor
+  module Extract
+    class TarGzTest < Minitest::Test
+      def setup
+        FileUtils.cp("test/fixtures/tar.tar.gz", "tmp/tar.tar.gz")
+      end
+
+      def teardown
+        FileUtils.rm_rf("tmp/tar")
+      end
+
+      def test_extract
+        refute File.exist?("tmp/tar/tar_dir/folder/tar_file")
+
+        extractor = Falkor::Extract::TarGz.new("tmp/tar.tar.gz")
+        assert_equal "tmp/tar", extractor.extract {}
+
+        assert File.exist?("tmp/tar/tar_dir/folder/tar_file")
+        refute File.exist?("tmp/tar.tar.gz")
+      end
+    end
+  end
+end
