4eb3dbd9690ba7b1401c34107dff9dc36096a409
Nick Pezza <npezza93@gmail.com>
2019-10-19 20:44:49 -0400

    Impl github actions

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gemspec
 
-gem "appraisal", "~> 2.2"
 gem "pry"
 gem "rubocop"
 gem "rubocop-performance"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "falkor"
+if ENV["COV"]
+  require "simplecov"
+
+  SimpleCov.start do
+    add_filter "/test/"
+  end
+end
 
 require "minitest/autorun"
+require "minitest/pride"
+require "pry"
+require "falkor"
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
